fd957d58a1ca39595db4f65874ed37091a6e11aa
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-10-18 18:58:03 +0200

    Upload only and cleaner script

--- cron.sh
+++ cron.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+BACKUP_DIR=/recovery/backups
+LOG_DIR=/recovery/logs
+TARGET="backup`date +%Y-%m-%d_%H-%M-%S`"
 
-exec &> "/recovery/logs/backup`date +%Y-%m-%d_%H-%M-%S`.log"
-tar --exclude='/recovery' --exclude='/proc' --exclude='/dev'--exclude='/snap' --exclude='/sbin' --exclude='/run' --exclude='/lib' --exclude='lib32' --exclude='/sys' --exclude='/tmp' --exclude='/var/lib'  --exclude='/boot' -cvpPzf "/recovery/backups/backup`date +%Y-%m-%d_%H-%M-%S`.taz.gz" /*
-onedrive -v --syncdir="/recovery"
-rm -f /recovery/backups/*
+exec &> "$LOG_DIR/$TARGET.log"
+mkdir $BACKUP_DIR/$TARGET
+tar --exclude='/recovery' --exclude='/proc' --exclude='/dev'--exclude='/snap' --exclude='/sbin' --exclude='/run' --exclude='/lib' --exclude='lib32' --exclude='/sys' --exclude='/tmp' --exclude='/var/lib'  --exclude='/boot' -cvpPzf "$BACKUP_DIR/$TARGET/$TARGET.taz.gz" /*
+onedrive -v --synchronize --upload-only --no-remote-delete --syncdir="$BACKUP_DIR/$TARGET"
+rm -f "$BACKUP_DIR/*"
