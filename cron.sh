5a8b75d8d16193203d2b1fc072ba9065bf8b0229
Hendrik Ulbrich <hendrikulbrich@outlook.de>
2018-10-19 11:26:25 +0200

    Support folder structure

--- cron.sh
+++ cron.sh
@@ -9,4 +9,4 @@ exec &> "$LOG_DIR/$TARGET.log"
 mkdir $BACKUP_DIR/$TARGET
 tar --exclude='/recovery' --exclude='/proc' --exclude='/dev'--exclude='/snap' --exclude='/sbin' --exclude='/run' --exclude='/lib' --exclude='lib32' --exclude='/sys' --exclude='/tmp' --exclude='/var/lib'  --exclude='/boot' -cvpPzf "$BACKUP_DIR/$TARGET/$TARGET.taz.gz" /*
 onedrive -v --synchronize --upload-only --no-remote-delete --syncdir="$BACKUP_DIR/$TARGET"
-rm -f "$BACKUP_DIR/*"
+rm -rf "$BACKUP_DIR/*"
